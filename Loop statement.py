dff4cc5e629eeb3ba3b3567650b515581c7c3b28
student <raina.tejveersingh11@gmail.com>
2026-05-04 13:51:24 +0530

    third commit

--- Loop statement.py
+++ Loop statement.py
@@ -12,6 +12,7 @@ for x in range(Start,End):
         print(x)"""
     
 #method 2
+"""
 start=int(input("Enter starting number "))
 end=int(input("Enter ending number "))
 if start%2==0:
@@ -19,6 +20,25 @@ if start%2==0:
         print(x,end=" ")
 else:
     for x in range(start+1,Stop,2):
-        print(x,end="")
+        print(x,end="")"""
     
-        
+"""
+start=int(input("Enter starting number "))
+end=int(input("Enter ending number "))
+if start%2!=0:
+    for x in range(start,end):
+        print(x)
+"""
+#wap to check prime number
+start=int(input("Enter number: "))
+c=0
+for x in range (1,start+1):
+    c=c+1
+    if c==2:
+        print(start,"is a prime number")
+    else:
+        print(start,"is a composite")
+#WAP to test the number entered armstrong or not
+num=int(input("Enter number to be tested"))
+
+
